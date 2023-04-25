5a7276127ec27bca4a3ef187e4cba39e71d43708
faithmwamkinga <faithmwamkinga@gmail.com>
2023-04-25 08:18:46 +0300

    Assessment2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,63 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main(){
+    //words("Amanda")
+    println(words("Amanda"))
+     multiples()
+    var currentAccount=CurrentAccount("789298738","Lumumba",300000)
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+}
+
+//Write and invoke one function that takes in a random string and returns the
+//following values: first character, last character, length of the string and
+//whether it begins with a vowel or not. (6pts)
+fun words(word:String):Array<Any>{
+  //var myWord="Amanda"
+    arrayListOf<Any>()
+    return word,
+
+   // return word.length(words)
+
+}
+
+
+//input string
+//output character 1st,2nd and last, length and if it startswith a vowel or not
+
+//3. Create a function that prints out multiples of 6 and 8 between 1 and 1000. For
+//multiples of both 6 and 8 it should print out “Bingo!” (5pts)
+
+fun multiples (numbers:Int) {
+
+    for (number in numbers)
+        if (number % 6 == 0 && number % 8 == 0)
+            println("Bingo")
+}
+
+//5. Create a class CurrentAccount with the following attributes: account number,
+//account name, balance. It has the following functions:
+//a. deposit(amount: Double) - Increments the balance by the amount
+//deposited
+//b. withdraw(amount: Double) - Decrements the balance by the amount
+//withdrawn
+//c. details() - Prints out the account number and balance and name in
+//this format: “Account number x with balance y is operated by z” (6pts)
+class CurrentAccount( var accountNumber: String,var accountName:String, var balance:Int  ) {
+    fun deposit(amount: Double, balance: Int): Double {
+        amount=2000.0
+        balance+=amount
+        return balance
+
+//        var newBalance = balance + amount
+//        return newBalance
+
+    }
+
+    fun withdraw(amount: Double): Int {
+        var withdrawn = 2000
+        withdrawn -= balance
+        return withdrawn
+    }
+
+    fun details(accountNumber: String, balance: Int, accountName: String): String {
+        println("Account number ${accountNumber} with balance ${balance} is operated by ${accountName}")
+    }
 }
